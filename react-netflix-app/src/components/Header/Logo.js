4f26df3a770f5790efe533481af816d0c9b45033
ksnd0297 <ksnd0297@gmail.com>
2023-01-16 17:07:35 +0900

    [feat] : header 레이아웃 구현 header에 들어가는 logo / nav / search / profile 관련 로직 구현

--- react-netflix-app/src/components/Header/Logo.js
+++ react-netflix-app/src/components/Header/Logo.js
@@ -4,7 +4,7 @@ import netflixLogo from "@assets/Netflix_2015_logo.svg";
 const Logo = () => {
   return (
     <div className="logo_container">
-      <img src={netflixLogo} />
+      <img src={netflixLogo} alt="넷플릭스 로고 이미지" />
     </div>
   );
 };
--- react-netflix-app/src/components/Header/Search.js
+++ react-netflix-app/src/components/Header/Search.js
@@ -1,5 +1,18 @@
+import "@scss/header/search.scss";
+
+import magnifyGlass from "@assets/magnifying-glass.svg";
+
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search_container">
+      <div className="input_container">
+        <div className="input_image">
+          <img src={magnifyGlass} alt="입력 돋보기 이미지" />
+        </div>
+        <input />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
