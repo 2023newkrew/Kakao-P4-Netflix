f4cdf352d5389d67a1461a9df0d96fe86567881c
musc.le <musc.le@kakaocorp.com>
2023-01-16 18:41:48 +0900

    feat: tailwind, twin-macro 설정 추가

--- react-netflix-app/tailwind.config.js
+++ react-netflix-app/tailwind.config.js
@@ -1,8 +1,34 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: {
+          fontSize: theme('fontSize.2xl'),
+          fontWeight: theme('fontWeight.bold'),
+        },
+        h2: {
+          fontSize: theme('fontSize.xl'),
+          fontWeight: theme('fontWeight.semibold'),
+        },
+        h3: {
+          fontSize: theme('fontSize.lg'),
+          fontWeight: theme('fontWeight.semibold'),
+        },
+        h3: {
+          fontSize: theme('fontSize.md'),
+          fontWeight: theme('fontWeight.semibold'),
+        },
+        input: {
+          outlineWidth: 0,
+        },
+      });
+    }),
+  ],
 };
