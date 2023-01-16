463ad9056ff24dca0d8296c2a644d0fb2a2d5cbc
musc.le <musc.le@kakaocorp.com>
2023-01-16 18:47:08 +0900

    feat: login 페이지 UI

--- react-netflix-app/src/router.js
+++ react-netflix-app/src/router.js
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/Main';
 import ErrorPage from './pages/Error';
-// import LoginPage from './pages/Login';
+import LoginPage from './pages/Login';
 
 const routes = [
   { path: '/', element: <MainPage />, errorElement: <ErrorPage /> },
-  // { path: '/login', element: <LoginPage /> },
+  { path: '/login', element: <LoginPage /> },
 ];
 const router = createBrowserRouter(routes);
 
