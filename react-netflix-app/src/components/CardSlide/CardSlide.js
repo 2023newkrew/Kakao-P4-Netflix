29ccefd12f909eec1e16435e736b1fc785452555
ksnd0297 <ksnd0297@gmail.com>
2023-01-18 00:26:32 +0900

    [feat] : overflow & button test cardSlide를 위한 overflow 와 button 로직 test

--- react-netflix-app/src/components/CardSlide/CardSlide.js
+++ react-netflix-app/src/components/CardSlide/CardSlide.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import MovieCard from "./MovieCard";
 
 import "@scss/cardSlide/cardSlide.scss";
@@ -6,6 +6,10 @@ import { API } from "../../utils/axios";
 
 const CardSlide = ({ title, genreId }) => {
   const [genreMovieList, setGenreMovieList] = useState([]);
+
+  const ref = useRef(null);
+  let currentSlideIndex = useRef(0).current;
+  let slideWidth = useRef(2240).current;
 
   useEffect(() => {
     fetchAndSetGenreMovieList();
@@ -19,10 +23,31 @@ const CardSlide = ({ title, genreId }) => {
   return (
     <div className="cardSlide_container">
       <div className="cardSlide_title">{title}</div>
+      {/* TODO : 밖으로 빼기 */}
       <div className="cardSlide_wrapper">
-        {genreMovieList.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
+        <button
+          className="cardSlide_leftButton"
+          onClick={() => {
+            currentSlideIndex -= 1;
+            ref.current.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
+          }}
+        >
+          A
+        </button>
+        <div className="cardSlide_slide" ref={ref}>
+          {genreMovieList.map((movie, index) => (
+            <MovieCard movie={movie} key={index} />
+          ))}
+        </div>
+        <button
+          className="cardSlide_rightButton"
+          onClick={() => {
+            currentSlideIndex += 1;
+            ref.current.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
+          }}
+        >
+          B
+        </button>
       </div>
     </div>
   );
