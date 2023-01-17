728a11ebcf06f3271526d32b0c69a2f8343c9f60
gray-sin <gray.sin@kakaocorp.com>
2023-01-17 17:19:32 +0900

    useReducer 적용

--- src/components/movie-list/index.jsx
+++ src/components/movie-list/index.jsx
@@ -1,28 +1,82 @@
 import './index.scss';
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useEffect, useReducer, useRef, useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from '../movie-card';
-import useScreenWidthSize from '../../hooks/useScreenWidthSize';
 
-const ScreenWidthSizeToDisplayNumber = {
-  xs: 2,
-  s: 3,
-  m: 4,
-  l: 5,
-  xl: 6,
+const reducer = (state, action) => {
+  const { length, page, displayNumber } = state;
+
+  switch (action.type) {
+    case 'page':
+    {
+      const { value } = action;
+      if (value < 0 || value > length - displayNumber) return state;
+      return { ...state, page: value };
+    }
+    case 'displayNumber':
+    {
+      const { value } = action;
+      if (page > length - value) {
+        return { ...state, page: length - value, displayNumber: value };
+      }
+      return { ...state, displayNumber: value };
+    }
+    default:
+      throw new Error();
+  }
 };
 
-function MovieList({ movies }) {
-  const screenWidthSize = useScreenWidthSize();
-  const displayNumber = ScreenWidthSizeToDisplayNumber[screenWidthSize];
+const ScreenWidthQuery = {
+  XS: '(max-width: 479px)',
+  S: '(min-width: 480px) and (max-width: 767px)',
+  M: '(min-width: 768px) and (max-width: 1023px)',
+  L: '(min-width: 1024px) and (max-width: 1279px)',
+  XL: '(min-width: 1280px)',
+};
 
-  const [page, setPage] = useState(0);
+const ScreenWidthQueryToDisplayNumber = {
+  [ScreenWidthQuery.XS]: 2,
+  [ScreenWidthQuery.S]: 3,
+  [ScreenWidthQuery.M]: 4,
+  [ScreenWidthQuery.L]: 5,
+  [ScreenWidthQuery.XL]: 6,
+};
+
+const createMqls = () => Object.values(ScreenWidthQuery).map(window.matchMedia);
+
+function MovieList({ movies }) {
+  const [mqls] = useState(createMqls);
+
+  const [{ page, length, displayNumber }, dispatch] = useReducer(
+    reducer,
+    {
+      page: 0,
+      length: movies.length,
+      displayNumber: ScreenWidthQueryToDisplayNumber[mqls.find(({ matches }) => matches).media],
+    },
+  );
+
+  useEffect(() => {
+    const handleChangeEvent = ({ matches, media }) => {
+      if (matches) {
+        dispatch({
+          type: 'displayNumber',
+          value: ScreenWidthQueryToDisplayNumber[media],
+        });
+      }
+    };
+
+    mqls.forEach((mql) => mql.addEventListener('change', handleChangeEvent));
+    return () => { mqls.forEach((mql) => mql.removeEventListener('change', handleChangeEvent)); };
+  }, []);
 
   const movieListElementRef = useRef(null);
-  const animateMoveListElement = (currentPage, nextPage) => {
+  const animateMoveListElement = (from, to) => {
     const keyframes = [
-      { transform: `translateX(calc(-1 * ${currentPage} * var(--item-and-gap-width)))` },
-      { transform: `translateX(calc(-1 * ${nextPage} * var(--item-and-gap-width)))` },
+      { transform: `translateX(calc(-1 * ${from} * var(--item-and-gap-width)))` },
+      { transform: `translateX(calc(-1 * ${to} * var(--item-and-gap-width)))` },
     ];
 
     const options = {
@@ -34,60 +88,59 @@ function MovieList({ movies }) {
     movieListElementRef.current.animate(keyframes, options);
   };
 
+  const maxPage = length - displayNumber;
+
   useEffect(() => {
     const handleKeydownEvent = (event) => {
       if (event.code === 'ArrowRight') {
-        setPage((currentPage) => {
-          const nextPage = currentPage + 1;
-          animateMoveListElement(currentPage, nextPage);
-          return nextPage;
-        });
+        if (page === maxPage) return;
+        const nextPage = page + 1;
+        dispatch({ type: 'page', value: nextPage });
+        animateMoveListElement(page, nextPage);
       }
 
       if (event.code === 'ArrowLeft') {
-        setPage((currentPage) => {
-          const nextPage = currentPage - 1;
-          animateMoveListElement(currentPage, nextPage);
-          return nextPage;
-        });
+        if (page === 0) return;
+        const nextPage = page - 1;
+        dispatch({ type: 'page', value: nextPage });
+        animateMoveListElement(page, nextPage);
       }
     };
 
     window.addEventListener('keydown', handleKeydownEvent);
     return () => window.removeEventListener('keydown', handleKeydownEvent);
-  }, []);
+  }, [page, maxPage]);
+
+  useEffect(() => {
+    animateMoveListElement(page, page);
+  }, [displayNumber]);
 
   return (
     <>
-      <div>
-        반응형 스크린 너비 사이즈:
-        {' '}
-        {screenWidthSize}
-      </div>
       <div>
         한 화면에 보이는 항목 수:
         {' '}
         {displayNumber}
       </div>
-      <div style={{ color: page < 0 || page > movies.length - displayNumber ? 'red' : 'unset' }}>
+      <div style={{ color: page < 0 || page > maxPage ? 'red' : 'unset' }}>
         페이지:
         {' '}
         {page}
         {' '}
         /
         {' '}
-        {movies.length - displayNumber}
+        {maxPage}
         {' '}
         (좌우 방향키로 변경)
       </div>
       <ul className="movie-list" ref={movieListElementRef}>
         {
-        movies.map((movie) => (
-          <li className="movie-list-item" key={movie.id}>
-            <MovieCard movie={movie} />
-          </li>
-        ))
-      }
+          movies.map((movie) => (
+            <li className="movie-list-item" key={movie.id}>
+              <MovieCard movie={movie} />
+            </li>
+          ))
+        }
       </ul>
     </>
   );
