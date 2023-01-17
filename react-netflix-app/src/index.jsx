f4990fb6bd1f14eca09a75f54e72a76af912077d
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-17 14:40:01 +0900

    chore: styled components 추가, 전역 스타일 설정

---
+++ react-netflix-app/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './styles/GlobalStyle';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+  </React.StrictMode>
+);
