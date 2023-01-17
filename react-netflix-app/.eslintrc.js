f3b48d637b37579e9382144d6f02618090a3d696
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-17 14:40:01 +0900

    chore: eslint, prettier 설정

---
+++ react-netflix-app/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
