64163d9ea5b627f8fb04213af3ef4edc8b0a8f70
ksnd0297 <ksnd0297@gmail.com>
2023-01-19 16:09:59 +0900

    [fix] : map 사용으로 인한 임시적 key 등록 임시적 index를 통해 map에 key 값 등록

--- react-netflix-app/src/components/CardSlide/CardSlide.js
+++ react-netflix-app/src/components/CardSlide/CardSlide.js
@@ -30,8 +30,8 @@ const CardSlide = ({ title }) => {
   // ! Hooks 는 조건이 있는 함수 내에 들어가면 안됨 MovieCard 내에서 if 로 index를 조회해서 선언하려 했으나 안되었고 props로 넘기는게 더 깔끔할 것 같음
 
   const movieCardList = genreMovieList.map((movie, index) => (
-    <SwiperSlide>
-      <MovieCard movie={movie} key={index} />
+    <SwiperSlide key={index}>
+      <MovieCard movie={movie} />
     </SwiperSlide>
   ));
 
