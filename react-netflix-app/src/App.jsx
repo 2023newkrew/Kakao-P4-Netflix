f4d3968e23fef0c9c12d77980f976b2f1413845d
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-19 14:59:30 +0900

    refactor: 메뉴 목록 별도 배열로 분리

--- react-netflix-app/src/App.jsx
+++ react-netflix-app/src/App.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import Header from '@components/Header';
 import Main from '@routes/Main';
 
+const menus = [
+  { id: 1, name: '홈' },
+  { id: 2, name: '시리즈' },
+  { id: 3, name: '영화' },
+  { id: 4, name: 'NEW! 요즘 대세 콘텐츠' },
+  { id: 5, name: '내가 찜한 콘텐츠' },
+  { id: 6, name: '언어별로 찾아보기' },
+];
+
 export default function App() {
   return (
     <>
-      <Header />
+      <Header menus={menus} />
       <Main />
     </>
   );
