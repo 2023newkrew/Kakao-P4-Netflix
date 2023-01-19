3f9aae748f533bf2b9bae43d581f7ddc581d263b
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-19 13:56:18 +0900

    chore: Update path alias @ craco, jsconfig

--- react-netflix-app/craco.config.js
+++ react-netflix-app/craco.config.js
@@ -8,6 +8,7 @@ module.exports = {
       '@apis': path.resolve(__dirname, 'src/apis/'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@configs': path.resolve(__dirname, 'src/configs/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
       '@constants': path.resolve(__dirname, 'src/constants/'),
       '@hooks': path.resolve(__dirname, 'src/hooks/'),
       '@utils': path.resolve(__dirname, 'src/utils/'),
