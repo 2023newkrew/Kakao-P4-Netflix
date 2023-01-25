d716be94aa16c587d8b02355933dd91dd3df435f
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-25 15:55:04 +0900

    feat: Show multiple movie list @ home

--- react-netflix-app/src/apis/home.js
+++ react-netflix-app/src/apis/home.js
@@ -10,6 +10,33 @@ export const getNowPlaying = async (page = 1) => {
   return response.data;
 };
 
+export const getPopular = async (page = 1) => {
+  const response = await Client.get('/movie/popular', {
+    params: {
+      page,
+    },
+  });
+  return response.data;
+};
+
+export const getToprated = async (page = 1) => {
+  const response = await Client.get('/movie/top_rated', {
+    params: {
+      page,
+    },
+  });
+  return response.data;
+};
+
+export const getUpcoming = async (page = 1) => {
+  const response = await Client.get('/movie/upcoming', {
+    params: {
+      page,
+    },
+  });
+  return response.data;
+};
+
 export const getDetail = async (id) => {
   const response = await Client.get(`/movie/${id}`);
   return response.data;
--- react-netflix-app/src/containers/home/index.jsx
+++ react-netflix-app/src/containers/home/index.jsx
@@ -3,20 +3,40 @@ import Footer from '@components/common/footer';
 import Header from '@components/common/header';
 import Hero from '@components/home/hero';
 import { useEffect, useState } from 'react';
-import { getNowPlaying } from '@apis/home';
+import { getNowPlaying, getPopular, getToprated, getUpcoming } from '@apis/home';
 import { rand } from '@/utils/math';
 import { MOVIE_LIST, MOVIE_LIST_TITLE } from '@/constants/movie';
 
 const Home = () => {
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [toprated, setToprated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+
   const [heroMovie, setHeroMovie] = useState({});
+
+  const movieLists = Object.entries({
+    [MOVIE_LIST.NOW_PLAYING]: nowPlaying,
+    [MOVIE_LIST.POPULAR]: popular,
+    [MOVIE_LIST.TOP_RATED]: toprated,
+    [MOVIE_LIST.UPCOMING]: upcoming,
+  });
 
   useEffect(() => {
     const fetchNowPlaying = async () => {
-      const data = await getNowPlaying();
-      const { results } = data;
+      const [nowPlayingData, popularData, topratedData, upcomingData] = await Promise.all([
+        getNowPlaying(),
+        getPopular(),
+        getToprated(),
+        getUpcoming(),
+      ]);
+
+      const { results } = nowPlayingData;
 
       setNowPlaying(results);
+      setPopular(popularData.results);
+      setToprated(topratedData.results);
+      setUpcoming(upcomingData.results);
       setHeroMovie(results[rand({ max: results.length }) - 1 || 0]);
     };
     fetchNowPlaying();
@@ -26,8 +46,9 @@ const Home = () => {
     <>
       <Header />
       <Hero movie={heroMovie} />
-      <MovieList title={MOVIE_LIST_TITLE[MOVIE_LIST.NOW_PLAYING]} movies={nowPlaying} />
-      {/* TODO: Fetch more movies and display it using .map */}
+      {movieLists.map(([title, movies]) => (
+        <MovieList key={title} title={MOVIE_LIST_TITLE[title]} movies={movies} />
+      ))}
       <Footer />
     </>
   );
