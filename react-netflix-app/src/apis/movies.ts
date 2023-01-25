39bfcd2f2fd9f406c4288bde8fe343c08699fe1f
hash.table <hash.table@kakaocorp.com>
2023-01-25 14:56:13 +0900

    feat: obj2queryString 함수 추가 및 API_PREFIX 생성

--- react-netflix-app/src/apis/movies.ts
+++ react-netflix-app/src/apis/movies.ts
@@ -1,11 +1,16 @@
+import { obj2queryString } from '@utils/queryString';
 import { customAxios } from './customAxios';
-const apiKey = process.env.REACT_APP_ACCESS_TOKEN;
+
+const API_PREFIX = obj2queryString({
+  language: 'ko-KR',
+  api_key: process.env.REACT_APP_ACCESS_TOKEN
+});
 
 export const getPopularMovies = async () => {
-  return await customAxios.get(`/movie/popular?language=ko-KR&api_key=${apiKey}`);
+  return await customAxios.get(`/movie/popular?${API_PREFIX}`);
 };
 
 export const getUpcomingMovies = async () => {
-  return await customAxios.get(`/movie/upcoming?language=ko-KR&api_key=${apiKey}`);
+  return await customAxios.get(`/movie/upcoming?${API_PREFIX}`);
 };
 
