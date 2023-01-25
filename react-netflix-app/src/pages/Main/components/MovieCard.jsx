a607f355274682abf6d403226f618302a84bb09f
은승균 <dmstmdrbs98@gmail.com>
2023-01-25 14:01:52 +0900

    chore: 영화 카드 컴포넌트 UI 수정

--- react-netflix-app/src/pages/Main/components/MovieCard.jsx
+++ react-netflix-app/src/pages/Main/components/MovieCard.jsx
@@ -8,10 +8,20 @@ import {
   DetailInfos,
   DetailContainer,
   Genres,
+  InfoRow,
+  UserVote,
+  UtilsWrapper,
+  UtilButton,
+  PlayButton,
+  MoreButton,
 } from '@pages/Main/components/MovieCard.style';
 import { THUMBNAIL_BASE_URL } from '@constants/tmdb';
 import { useModal } from '@components/Modal';
 import MovieDetail from '@pages/Main/[id]';
+import playIcon from '@assets/icons/play.svg';
+import plusIcon from '@assets/icons/plus.svg';
+import thumbsUpIcon from '@assets/icons/thumbsUp.svg';
+import arrowDownIcon from '@assets/icons/arrowDown.svg';
 
 const useMovieDetail = (movieId) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -54,17 +64,33 @@ const DetailMovieCard = ({ movie }) => {
   };
 
   return (
-    <DetailContainer
-      className="movie-detail"
-      onClick={() => {
-        showMovieDetailModal();
-      }}
-    >
+    <DetailContainer className="movie-detail" onClick={showMovieDetailModal}>
       <ThumbnailContainer>
         <ThumbnailImage src={THUMBNAIL_BASE_URL + movie.backdrop_path} alt="썸네일" />
       </ThumbnailContainer>
       <DetailInfos>
         <h4>{movie.title}</h4>
+        <UtilsWrapper
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        >
+          <PlayButton>
+            <img src={playIcon} alt="재생 아이콘" height="18" width="18" />
+          </PlayButton>
+          <UtilButton>
+            <img src={plusIcon} alt="추가 아이콘" height="18" width="18" />
+          </UtilButton>
+          <UtilButton>
+            <img src={thumbsUpIcon} alt="좋아요 아이콘" height="18" width="18" />
+          </UtilButton>
+          <MoreButton onClick={showMovieDetailModal}>
+            <img src={arrowDownIcon} alt="더보기 아이콘" height="18" width="18" />
+          </MoreButton>
+        </UtilsWrapper>
+        <InfoRow>
+          {!isLoading && detail?.vote_average && <UserVote>{Math.round(detail.vote_average * 10)}점</UserVote>}
+        </InfoRow>
         <Genres>{!isLoading && detail?.genres.map((genre) => genre.name).join(' / ')}</Genres>
       </DetailInfos>
     </DetailContainer>
--- react-netflix-app/src/pages/Main/components/MovieCard.style.js
+++ react-netflix-app/src/pages/Main/components/MovieCard.style.js
@@ -17,7 +17,13 @@ absolute top-0 left-0 w-full h-full object-cover
 export const DetailContainer = tw.div`
   cursor-pointer transition-transform [transition-duration: 300ms] [transition-delay: 100ms] absolute top-0 right-0 left-0 invisible hover:visible hover:scale-125 hover:-translate-y-12 hover:origin-bottom hover:z-[2]
 `;
+export const UtilsWrapper = tw.div`relative w-full flex items-center mt-2`;
+export const UtilButton = tw.button`h-10 w-10 rounded-full mr-2 text-white border-gray-300 hover:border-gray-100 border-[1.5px] bg-[#2c2c2c] flex items-center justify-center text-xs`;
+export const PlayButton = tw(UtilButton)`bg-white hover:bg-gray-100 text-[#2c2c2c]`;
+export const MoreButton = tw(UtilButton)`absolute right-0`;
 export const DetailInfos = tw.div`
-  bg-black w-full h-[70px] p-2 flex flex-col justify-between
+  bg-black w-full p-2 flex flex-col justify-between
 `;
+export const InfoRow = tw.div`w-full flex items-center my-2 bg-inherit`;
+export const UserVote = tw.p`text-sm text-[#45d369]`;
 export const Genres = tw.p`flex items-start flex-wrap h-auto bg-black text-sm font-light`;
