a3ea0ee4029ff0ff93a90b883f41fdc500cf127c
은승균 <dmstmdrbs98@gmail.com>
2023-01-26 14:51:57 +0900

    feat: 헤더 검색 레이아웃 추가

---
+++ react-netflix-app/src/components/SearchInput.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef } from 'react';
+import tw from 'twin.macro';
+import PropTypes from 'prop-types';
+import { ReactComponent as SearchIcon } from '@icons/search.svg';
+import useInput from '@utils/hooks/useInput';
+
+export const Container = tw.div`
+  flex items-center h-10  bg-[rgb(0,0,0,40)] border border-white px-2
+`;
+export const Input = tw.input`
+  bg-transparent h-full text-[14px] outline-0 ml-2
+`;
+
+const SEARCH_DELAY_MS = 300;
+const useSearch = () => {
+  const timer = useRef(null);
+  const { inputValue, handleChange } = useInput();
+
+  const search = (value) => {
+    // TODO: 검색 API
+  };
+
+  useEffect(() => {
+    clearTimeout(timer.current);
+    if (!inputValue) {
+      return;
+    }
+
+    timer.current = setTimeout(() => {
+      search(inputValue);
+    }, SEARCH_DELAY_MS);
+  }, [inputValue]);
+
+  return {
+    inputValue,
+    handleChange,
+  };
+};
+
+const SearchInput = ({ setCanSearch }) => {
+  const searchInputRef = useRef(null);
+  const { inputValue, handleChange } = useSearch();
+
+  return (
+    <Container>
+      <SearchIcon />
+      <Input
+        ref={searchInputRef}
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="제목"
+        onBlur={() => {
+          if (searchInputRef.current.value) {
+            return;
+          }
+          setCanSearch(false);
+        }}
+      />
+    </Container>
+  );
+};
+SearchInput.propTypes = {
+  setCanSearch: PropTypes.func.isRequired,
+};
+export default SearchInput;
