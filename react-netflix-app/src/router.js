fb1519fdff398f8b0f5fb88c55437ac3d44c853d
은승균 <dmstmdrbs98@gmail.com>
2023-01-26 18:42:38 +0900

    feat: 검색 API 연결

--- react-netflix-app/src/router.js
+++ react-netflix-app/src/router.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '@pages/Main/MainPage';
 import ErrorPage from '@pages/Error';
 import LoginPage from '@pages/Login';
-import SearchPage from '@pages/Search/index';
+import SearchPage from '@pages/Search/SearchPage';
 import App from './App';
 
 const routes = [
