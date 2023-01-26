a796da2126ee82c5f420735ce6327f0ecbaa015a
은승균 <dmstmdrbs98@gmail.com>
2023-01-26 16:35:30 +0900

    feat: 스크롤 복구 기능 추가

--- react-netflix-app/src/utils/hooks/useBodyScrollLock.js
+++ react-netflix-app/src/utils/hooks/useBodyScrollLock.js
@@ -1,11 +1,16 @@
 import { useEffect, useRef } from 'react';
 
 const useBodyScrollLock = (isLocked) => {
+  const scrollY = useRef(0);
   const root = useRef(document.body.querySelector('#root'));
   useEffect(() => {
     if (!isLocked) {
+      document.body.style.removeProperty('overflow');
       root.current.style.removeProperty('overflow-y');
+      window.scroll({ top: scrollY.current, behavior: 'smooth' });
     } else {
+      scrollY.current = window.scrollY;
+      document.body.style.setProperty('overflow', 'hidden');
       root.current.style.setProperty('overflow-y', 'hidden');
     }
   }, [isLocked]);
