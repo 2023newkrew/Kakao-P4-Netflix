2682ff7104fe7cdc17f858c1b3d83c5530294c09
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-30 18:24:55 +0900

    feat: Create search container & route

---
+++ react-netflix-app/src/containers/search/index.jsx
@@ -0,0 +1,11 @@
+import Footer from '@/components/common/footer';
+import Header from '@/components/common/header';
+
+const Search = () => (
+  <>
+    <Header />
+    <Footer />
+  </>
+);
+
+export default Search;
--- react-netflix-app/src/routes/app-router/index.jsx
+++ react-netflix-app/src/routes/app-router/index.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { ROUTE, ROUTE_PATH } from '@/constants/route';
 import Home from '@/containers/home';
 import MovieRouter from '../movie-router';
+import Search from '@/containers/search';
 
 const AppRouter = () => (
   <Routes>
     <Route path={ROUTE_PATH[ROUTE.HOME]} element={<Home />} />
     <Route path={`${ROUTE_PATH[ROUTE.MOVIE]}/*`} element={<MovieRouter />} />
+    <Route path={`${ROUTE_PATH[ROUTE.SEARCH]}`} element={<Search />} />
   </Routes>
 );
 
