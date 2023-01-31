b25e36ed6b816873fc07a54bef5e0bcbe3feea8b
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-31 15:03:25 +0900

    feat: 라우팅 경로 설정, 404 페이지

--- react-netflix-app/src/App.jsx
+++ react-netflix-app/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '@components/Header';
-import Main from '@routes/Main';
+import NotFound from '@routes/NotFound';
+import Main from '@/routes';
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
-    </>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
