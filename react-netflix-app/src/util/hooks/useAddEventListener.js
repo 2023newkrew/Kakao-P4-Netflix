220fd221937ab1ba7f3990178e2590f7f26f5698
hape.k <hape.k@kakaocorp.com>
2023-01-31 19:16:01 +0900

    [수정] eslint 경고 해결

--- react-netflix-app/src/util/hooks/useAddEventListener.js
+++ react-netflix-app/src/util/hooks/useAddEventListener.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function useAddEventListener(
   ref,
@@ -7,13 +7,15 @@ export default function useAddEventListener(
   condition = true,
   beforeAttachEventCallback = null
 ) {
+  const handlerRef = useRef(handler);
   useEffect(() => {
     if (!condition) return;
 
     const target = ref.current;
+    const handlerRefTarget = handler.current;
     if (beforeAttachEventCallback) beforeAttachEventCallback();
-    target.addEventListener(eventType, handler);
+    target.addEventListener(eventType, handlerRef.current);
 
-    return () => target.removeEventListener(eventType, handler);
-  }, []);
+    return () => target.removeEventListener(eventType, handlerRefTarget);
+  }, [beforeAttachEventCallback, condition, eventType, handler, ref]);
 }
