322f80b9519bfca4d4998d428506d02702db5598
ksnd0297 <ksnd0297@gmail.com>
2023-02-02 08:45:42 +0900

    [fix]  : 영화 제목으로 인해 header 가 제대로 보이지 않는 현상 수정

--- react-netflix-app/src/styles/header/Header.style.js
+++ react-netflix-app/src/styles/header/Header.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const HeaderContainer = styled.div`
   width: 100%;
   height: 6%;
-  background: linear-gradient(180deg, black, 40%, transparent);
+  background: #141414;
   position: fixed;
 
   padding: 0 60px;
