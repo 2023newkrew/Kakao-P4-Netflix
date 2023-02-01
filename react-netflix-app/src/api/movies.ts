834894085f0446e896636bcf5f4b481fcc6b80e9
은승균 <dmstmdrbs98@gmail.com>
2023-02-01 17:37:26 +0900

    chore: API 관련 코드 ts 전환

---
+++ react-netflix-app/src/api/movies.ts
@@ -0,0 +1,31 @@
+import { Movie } from '@/types/movie';
+import httpClient from '@api/http';
+import { AxiosResponse } from 'axios';
+
+const MOVIE_URL = '/movie';
+
+const fetcher = (path: string, params?: object) => httpClient.get(MOVIE_URL + path, { params });
+
+interface PagedResponse {
+  page: number;
+  results: any;
+  total_pages: number;
+  total_results: number;
+}
+export interface MovieResponse extends PagedResponse {
+  results: Movie;
+}
+export interface MoviesResponse extends PagedResponse {
+  results: Movie[];
+}
+type ApiResponse<T> = Promise<AxiosResponse<T>>;
+export type MoviesApiResponse = ApiResponse<MoviesResponse>;
+export type MovieApiResponse = ApiResponse<MovieResponse>;
+
+export const getPopularMovies = (): MoviesApiResponse => fetcher('/popular');
+export const getTopRatedMovies = (): MoviesApiResponse => fetcher('/top_rated');
+export const getUpcomingMovies = (page = 1): MovieApiResponse => fetcher('/upcoming', { page });
+
+export const getMovieDetail = (movieId: string) => fetcher(`/${movieId}`);
+export const getSimilarMovie = (movieId: string) => fetcher(`/${movieId}/similar`);
+export const getMovieVideos = (movieId: string) => fetcher(`/${movieId}/videos`);
