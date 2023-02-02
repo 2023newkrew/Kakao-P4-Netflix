c4377d7b511e64bf66265944d0a1f3a872f4214b
Kim Seung Hwan <dev.appmin@gmail.com>
2023-02-02 14:13:02 +0900

    feat: Implement movie container

---
+++ react-netflix-app/src/containers/movie/[id]/styles.js
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const MovieContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
+
+export const MovieImage = styled.img`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+`;
+
+export const MovieTitle = styled.h1`
+  position: absolute;
+  bottom: 16px;
+  left: 16px;
+  z-index: 100;
+`;
