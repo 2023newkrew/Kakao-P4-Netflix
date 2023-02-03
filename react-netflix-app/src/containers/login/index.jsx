b40c5044b6271582263a8dee034bd477d9e3c42e
Kim Seung Hwan <dev.appmin@gmail.com>
2023-02-03 13:38:55 +0900

    feat: Create register text component

--- react-netflix-app/src/containers/login/index.jsx
+++ react-netflix-app/src/containers/login/index.jsx
@@ -1,15 +1,10 @@
-import { Link } from 'react-router-dom';
-import { ROUTE, ROUTE_PATH } from '@/constants/route';
-
 import LoginForm from '@/components/login/login-form';
+import RegisterText from '@/components/login/register-text';
 
 const Login = () => (
   <>
     <LoginForm />
-    <p>
-      Don&apos;t have an account?{' '}
-      <Link to={`${ROUTE_PATH[ROUTE.ACCOUNT]}/${ROUTE_PATH[ROUTE.REGISTER]}`}>Register</Link>
-    </p>
+    <RegisterText />
   </>
 );
 
